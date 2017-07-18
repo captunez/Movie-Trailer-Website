652f1aee453fa473a27c44fc08b450d6c7385c24
张 家豪 <zhangkagou@zhangdeMacBook-Air-2.local>
2017-07-18 11:31:46 +0800

    Add comments for the movie class in media.py

--- media.py
+++ media.py
@@ -1,5 +1,7 @@
 class Movie():
+
+	# Constructor for Class Movie
     def __init__(self, title, poster_image_url, trailer_youtube_url):
-        self.title = title
-        self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
+        self.title = title   # movie title
+        self.poster_image_url = poster_image_url  # movie poster image URL
+        self.trailer_youtube_url = trailer_youtube_url # movie trailer Youtube URL
